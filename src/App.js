b608a4f3cef58ea7042ff04a4ab16edb985e4398
Akphecheat <akphecheat.te@gmail.com>
2021-05-31 15:56:31 +0700

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './style.css';
+import Greet from './components/Greet';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
+        <Greet />
       </div>
     );
   }
