605d2b9485e865d5c3b1ccad0c505399acb1a1df
Akphecheat <akphecheat.te@gmail.com>
2021-06-01 13:30:43 +0700

    1 file modified

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -9,10 +9,12 @@ class Counter extends Component {
   }
 
   increment() {
-    this.setState({
-      count: this.state.count + 1
-    });
-    console.log(this.state.count);
+    this.setState(
+      {
+        count: this.state.count + 1
+      },
+      () => console.log(this.state.count)
+    );
   }
 
   decrement() {
