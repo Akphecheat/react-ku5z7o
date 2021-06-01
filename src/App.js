d41bd7005ea6e2c1a61ccdabdb1c7d9bbd234171
Akphecheat <akphecheat.te@gmail.com>
2021-06-01 11:03:02 +0700

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -3,21 +3,13 @@ import logo from './logo.svg';
 import './style.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
+import Message from './components/Message';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* functional componnent */}
-        <Greet name="Bona" job="Banker" />
-        <Greet name="Kosal" job="IT" />
-        <Greet name="Vannak" job="Designer" />
-        {/* <Welcome /> */}
-
-        {/* class component */}
-        <Welcome name="Bona" job="Banker" />
-        <Welcome name="Kosal" job="IT" />
-        <Welcome name="Vannak" Job="Designer" />
+        <Message />
       </div>
     );
   }
