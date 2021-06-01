1ee79a3d5f64ba2bb25f2c6d0b4f20d86eb145e0
Akphecheat <akphecheat.te@gmail.com>
2021-06-01 13:24:04 +0700

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import './style.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Message from './components/Message';
+import Counter from './components/Counter';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Message />
+        <Counter />
       </div>
     );
   }
